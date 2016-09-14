19a5e5c2c36135437713edfeacfb6eebcc88ddaa
karabanb <b.karaban@me.com>
2016-09-14 20:14:29 +0200

    Accuracy 0.77512

--- titanic.R
+++ titanic.R
@@ -54,7 +54,7 @@ in.training$Embarked[c(62,830)]<-"C"
 set.seed(1234)
 in.training<-combi[1:891,]
 in.testing<-combi[892:nrow(combi),]
-tree_1<-rpart(Survived~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked + Title + Tile_2 + FamilySize + Deck, data=in.training, method = "class")
+tree_1<-rpart(Survived~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked + Title + FamilySize + Deck, data=in.training, method = "class")
 predicted.test_1<-predict(tree_1,in.testing, type = "class")
 predicted.train<-predict(tree_1,in.training, type = "class")
 
@@ -63,32 +63,4 @@ write.csv(my_solution_1,"tree_1_Titanic.csv",row.names = F)
 conf.matrix_bench<-table(predicted.train,in.training$Survived)
 print(conf.matrix_bench)
 
-#  --Acc: train: 0.83950  test: 0.78469
-tree_2<-rpart(Survived~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked, data = in.training, method = "class")
-predicted.test_2<-predict(tree_2, in.testing, type = "class")
-predicted.train_2<-predict(tree_2, in.training, type="class")
-
-my_solution<-data.frame(PassengerID=test$PassengerId, Survived=predicted.test_2)
-write.csv(my_solution,file="tree_2_Titanic.csv", row.names = FALSE)
-
-conf.matrix_2<-table(predicted.train_2,in.training$Survived)
-print(conf.matrix_2)
-
-
-## Tree with cp=0.005
-
-#--Acc: 0.8518519 test: 0.78469
-
-control<-rpart.control(cp=0.0058, minsplit = 1)
-tree_3<-rpart(Survived~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked + Title + Tile_2 + FamilySize + Deck, data=in.training, method = "class", control = control)
-predicted.train_3<-predict(tree_3, in.training, type = "class")
-conf.matrix_3<-table(predicted.train_3, in.training$Survived)
-print(conf.matrix_3)
-printcp(tree_3)
-print(conf.matric)
-
-rpart.plot(tree_3, extra=1)
-predicted_test_3<-predict(tree_3,in.testing, type="class")
-my.solution_3<-data.frame(PassengerID=in.testing$PassengerId,Survived=predicted_test_3)
-write.csv(my.solution_3,"tree_3_Titanic.csv", row.names = F)
 
